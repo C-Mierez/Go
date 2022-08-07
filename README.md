39b3e19796ab5cbe41ae9defb23acaa51f2c4bb0
C-Mierez <carlos.mierez20@gmail.com>
2022-08-07 00:05:42 -0300

    Added README and more variable cases

---
+++ README.md
@@ -0,0 +1,13 @@
+# Go<sub>lang</sub> 🐿
+
+> First time ever learning Go.
+> Writing down all my finding about the language in here for future reference.
+
+### Packages
+
+...
+
+### Variables must be used
+
+Programs will **not** compile if there are variables that are not used. This is an opinionated design decision from the language itself.
+
--- main.go
+++ main.go
@@ -7,14 +7,35 @@ func main() {
 }
 
 func variables() {
+	// Normal way of declaring variables
 	var a int
 	var b bool = true
-	var c = "inferredStr"
-	d := "inferredAndDeclaredStr"
 	a = 10
+
+	// Inferring the type of a variable from its first value
+	var c = "inferredStr"
+
+	// Declaring and inferring the type of a variable at the same time from the first value
+	d := "inferredAndDeclaredStr"
+	f := 9.
+
+	// Declaring multiple variables at once
+	// Usually for variables that are related to each other but independent to everything else
+	var (
+		name    string = "Charles"
+		age     int    = 60
+		isAlive bool   = true
+	)
 
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
 	fmt.Println(d)
+
+	// Printing out the value and type of a variable
+	fmt.Printf("%v, %T \n", d, d)
+	fmt.Printf("%v, %T \n", f, f) // Value of `f` declared and initialized as Float
+
+	fmt.Printf("Name: %v, Age: %v, Alive: %v, \n", name, age, isAlive)
+
 }
