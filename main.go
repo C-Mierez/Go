d3a9fc638b5bc32436ece800cd75ab4a64407d0a
C-Mierez <carlos.mierez20@gmail.com>
2022-08-06 23:48:04 -0300

    Different declarations and assignment

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	variables()
+}
+
+func variables() {
+	var a int
+	var b bool = true
+	var c = "inferredStr"
+	d := "inferredAndDeclaredStr"
+	a = 10
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 }
